aac1785a82264276575f5d2adc94cebae99e0d8c
melsener <melsener@users.noreply.github.com>
2026-03-16 22:09:56 +0100

    progress 25/94

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,16 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count == 1 {
+        2
+    } else if count <= 40 {
+        count * 2
+    } else {
+        count
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
